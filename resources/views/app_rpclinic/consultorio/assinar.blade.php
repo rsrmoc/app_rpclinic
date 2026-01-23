f0a9996f7a8f1af28e16968d0172b2a5ac56c7ac
yuriwinchest <yuriallmeida@gmail.com>
2026-01-22 23:04:14 -0300

    UI: Adiciona espacamento vertical no calendario de Documentos

--- resources/views/app_rpclinic/consultorio/assinar.blade.php
+++ resources/views/app_rpclinic/consultorio/assinar.blade.php
@@ -127,6 +127,11 @@
             font-size: 0.8rem !important;
         }
 
+        .air-datepicker-body--cells.-days- {
+            grid-template-columns: repeat(7, 1fr) !important;
+            row-gap: 8px !important; /* Increase vertical spacing */
+        }
+
         .air-datepicker-cell {
             color: #64748b !important; /* slate-500 */
             font-size: 0.9rem !important;
